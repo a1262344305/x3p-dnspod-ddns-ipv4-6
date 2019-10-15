8a5ef632a1fb6e6f6ff7300a2ede9676de162af0
方块君 <2991965574@qq.com>
2019-10-15 19:47:40 +0800

    ddns_ipv6.py

---
+++ ddns_ipv6.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import httplib
+import urllib
+import socket
+import time
+import ssl
+import os
+import re
+
+ssl._create_default_https_context = ssl._create_unverified_context
+params = dict(
+    login_token="ID,TOKEN",  # replace with your API token, see https://support.dnspod.cn/Kb/showarticle/tsid/227/
+    format="json",
+    domain_id=23333,  # 替换成你获取的
+    record_id=23333,  # 替换成你获取的
+    sub_domain="www",  # 替换成你的二级域名
+    record_line="默认",
+    record_type="AAAA",
+)
+current_ip = None
+
+
+def ddns(ip):
+    params.update(dict(value=ip))
+    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/json"}
+    conn = httplib.HTTPSConnection("dnsapi.cn")
+    conn.request("POST", "/Record.Modify", urllib.urlencode(params), headers)
+
+    response = conn.getresponse()
+    print(response.status, response.reason)
+    data = response.read()
+    print(data)
+    conn.close()
+    return response.status == 200
+
+
+def getip():
+    string_IPv6 = os.popen('ifconfig eth0 | grep -w "global"').readlines()
+    searchResult=re.search("s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*",string_IPv6[0])
+    retun = searchResult.group(1)
+    return retun
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            ip = getip()
+            print(ip)
+            if current_ip != ip:
+                if ddns(ip):
+                    current_ip = ip
+        except Exception, e:
+            print(e)
+            pass
+        time.sleep(60)
